d74b7ad56dcaf765eaf7c614616d5badb6d6ebc9
Froxss <burakyetis@gmail.com>
2023-09-30 18:19:07 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'matchers';
+  inputValue = '';
+  burak = 'correct';
+  randomText = faker.lorem.sentence();
+
+  getInputValue(value: string) {
+    this.inputValue = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
